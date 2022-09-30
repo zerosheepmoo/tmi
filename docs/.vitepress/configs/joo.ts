d0ed8e31caa25bb753447115e3708bd69dc9796f
Joo <dlaaudwls102@gmail.com>
2022-09-30 17:31:06 +0900

    joo

--- docs/.vitepress/configs/joo.ts
+++ docs/.vitepress/configs/joo.ts
@@ -1,5 +1,11 @@
 import { DefaultTheme } from "vitepress/types/default-theme";
 
 export const sidebars: DefaultTheme.SidebarMulti = {
-    "/joo": [],
+    "/joo": [
+        {
+            text: "Memo",
+            collapsed: true,
+            items: [{ text: "Overview", link: "/joo/index" }],
+        },
+    ],
 };
